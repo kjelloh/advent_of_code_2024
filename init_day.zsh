d421df4204b738b2013cbbb5a9dbb9741e801652
kjelloh <kjell-olov.hogdahl@itfied.se>
2024-12-01 13:36:59 +0100

    init_day.zsh from overwriting existing files and added CmakeLists.txt to day_1

--- init_day.zsh
+++ init_day.zsh
@@ -22,20 +22,16 @@ fi
 # Create the new folder (day_n)
 mkdir -p "$NEW_FOLDER"
 
-# Copy main.cpp and CMakeLists.txt from the template folder to the new folder
-cp "$TEMPLATE_FOLDER/day_0.cpp" "$NEW_FOLDER/day_$DAY_NUMBER.cpp"
-cp "$TEMPLATE_FOLDER/CMakeLists.txt" "$NEW_FOLDER/"
-cp "$TEMPLATE_FOLDER/init_tool_chain.zsh" "$NEW_FOLDER/"
-cp "$TEMPLATE_FOLDER/pull_data.zsh" "$NEW_FOLDER/"
+# Copy main.cpp, CMakeLists.txt etc. from the template folder to the new folder
+cp -nv "$TEMPLATE_FOLDER/day_0.cpp" "$NEW_FOLDER/day_$DAY_NUMBER.cpp"
+cp -nv "$TEMPLATE_FOLDER/CMakeLists.txt" "$NEW_FOLDER/"
+cp -nv "$TEMPLATE_FOLDER/init_tool_chain.zsh" "$NEW_FOLDER/"
+cp -nv "$TEMPLATE_FOLDER/pull_data.zsh" "$NEW_FOLDER/"
 touch "$NEW_FOLDER//example.txt"
-
 
 # Create a day.txt file in the new sub-folder with the provided day number
 echo "$DAY_NUMBER" > "$NEW_FOLDER/day.txt"
 echo "day.txt created in $NEW_FOLDER with value: $DAY_NUMBER"
 
 # Confirm the files have been copied
-echo "Files copied to $NEW_FOLDER"
-
-cd "$NEW_FOLDER"
-pwd
+echo "You can now cd to $NEW_FOLDER and do ./init_tool_chain.zsh and ./pull_data.zsh"
