1c3f80071360a9adcf0513c7ac6999d62402cd10
kjelloh <kjell-olov.hogdahl@itfied.se>
2024-12-02 16:53:01 +0100

    Changed day_0 template to execute part 1 test, part 1 puzzle, part 2 test and part 2 puzzle and show execution times

--- day_0/day_0.cpp
+++ day_0/day_0.cpp
@@ -17,71 +17,9 @@
 #include <numeric> // E.g., std::accumulate
 #include <limits> // E.g., std::numeric_limits
 #include <fstream>
-// #include <experimental/generator> // supported by visual studio 2022 17.8.2 with project setting/compiler switch /std:c++latest
 #include <format>
 #include <optional>
 #include <regex>
-
-std::pair<std::string_view, std::vector<std::string_view>> split_string_view(std::string_view input, char separator) {
-    std::vector<std::string_view> parts;
-    size_t start = 0;
-    size_t end;
-
-    // Find each substring separated by the separator
-    while ((end = input.find(separator, start)) != std::string_view::npos) {
-        parts.emplace_back(input.substr(start, end - start));
-        start = end + 1;
-    }
-
-    // Add the last substring
-    parts.emplace_back(input.substr(start));
-
-    // Return the original string view and the vector of parts
-    return {input, parts};
-}
-
-constexpr int NonMatching = -1; // Constant for non-matching tokens
-
-std::pair<std::string, std::vector<std::string_view>> split_string_regex(const std::string& input, const std::string& regex_pattern) {
-    std::vector<std::string_view> parts;
-    std::regex regex(regex_pattern);
-
-    // Using the constant NonMatching for clarity
-    auto begin = std::sregex_token_iterator(input.begin(), input.end(), regex, NonMatching);
-    auto end = std::sregex_token_iterator();
-
-    for (auto it = begin; it != end; ++it) {
-        // Create a string_view on each segment based on the original string
-        std::string_view part(input.data() + (it->first - input.begin()), it->length());
-        parts.emplace_back(part); // Add the string_view to parts
-    }
-
-    // Return the original string and the vector of parts
-    return {input, parts};
-}
-
-std::pair<std::string_view, std::vector<std::string_view>> find_overlapping_matches(std::string_view input, std::string const& regex_pattern) {
-    std::vector<std::string_view> matches; // Vector to hold the matches
-    auto to_windowed_regex = [](std::string const& regex) -> std::string {
-      if (regex.size()>0 and regex[0] != '^') return std::string{"^"} + regex;
-      return regex;
-    };
-    std::regex regex{to_windowed_regex(regex_pattern)};
-    std::match_results<std::string_view::const_iterator> match; // Match results holder
-
-    for (int position = 0;position < input.size();++position) {
-      if (std::regex_search(input.begin() + position, input.end(), match, regex)) {
-        std::println("matched '{}' at position:{}",match.str(),position);
-        matches.emplace_back(input.substr(position,match[0].length())); // Store matched substring as string_view
-      }
-    }
-  
-    // Return the original input and the vector of matches
-    return {input, matches}; // Return original string and matches as string_views
-}
-
-
-char const* example = R"()";
 
 auto const NL = "\n";
 auto const T = "\t";
@@ -89,10 +27,6 @@ auto const NT = "\n\t";
 
 using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64 bit int: 9.22 x 10^18
 using Result = Integer;
-using Answer = std::pair<std::string, Result>;
-using Answers = std::vector<Answer>;
-using Solution = std::map<int, Answers>; // Puzzle part -> Answers
-
 using Model = std::vector<std::string>;
 
 Model parse(auto& in) {
@@ -107,65 +41,86 @@ Model parse(auto& in) {
   return result;
 }
 
+using Args = std::vector<std::string>;
+
 namespace part1 {
-  Result solve_for(Model& model,auto args) {
-    Result result{};
+  std::optional<Result> solve_for(std::istream& in,Args const& args) {
+    std::optional<Result> result{};
     std::cout << NL << NL << "part1";
+    if (in) {
+      auto model = parse(in);
+    }
     return result;
   }
 }
 
 namespace part2 {
-  Result solve_for(Model& model,auto args) {
-    Result result{};
+  std::optional<Result> solve_for(std::istream& in,Args const& args) {
+    std::optional<Result> result{};
     std::cout << NL << NL << "part2";
+    if (in) {
+      auto model = parse(in);
+    }
     return result;
   }
 }
 
+using Answers = std::vector<std::pair<std::string,std::optional<Result>>>;
 int main(int argc, char *argv[])
 {
-  Solution solution{};
-  std::cout << NL << "argc : " << argc;
-  for (int i = 0; i < argc; ++i) {
-    std::cout << NL << "argv[" << i << "] : " << std::quoted(argv[i]);
+  Args args{};
+  for (int i=0;i<argc;++i) {
+    args.push_back(argv[i]);
   }
-  // day_n x y
-  std::tuple<int,std::string> args{1,"example.txt"};
-  auto& [part,file] = args;
-  if (argc > 1 ) {
-    part = std::stoi(argv[1]);
-    if (argc > 2) {
-      file = argv[2];
+  Answers answers{};
+  std::vector<std::chrono::time_point<std::chrono::system_clock>> exec_times{};
+  exec_times.push_back(std::chrono::system_clock::now());
+  for (int state=0;state<4;++state) {
+    switch (state) {
+      case 0: {
+        std::filesystem::path file{"../../example.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 1 Test",part1::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 1: {
+        std::filesystem::path file{"../../puzzle.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 1     ",part1::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 2: {
+        std::filesystem::path file{"../../example.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 2 Test",part2::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 3: {
+        std::filesystem::path file{"../../puzzle.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 2     ",part2::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      default:{std::cerr << "\nSORRY, no action for state " << state;} break;
     }
   }
-  constexpr std::string input_folder{"../../"};
-  file = input_folder + file;
-  std::cout << NL << "Part=" << part << " file=" << std::quoted(file);
-  std::ifstream in{ file };
-  auto model = parse(in);
-
-  switch (part) {
-  case 1: {
-    auto answer = part1::solve_for(model,args);
-    solution[part].push_back({ file,answer });
-  } break;
-  case 2: {
-    auto answer = part2::solve_for(model,args);
-    solution[part].push_back({ file,answer });
-  } break;
-  default:
-    std::cout << NL << "No part " << part << " only part 1 and 2";
+  
+  std::cout << "\n\nANSWERS";
+  for (int i=0;i<answers.size();++i) {
+    std::cout << "\nduration:" << std::chrono::duration_cast<std::chrono::milliseconds>(exec_times[i+1] - exec_times[i]).count() << "ms";
+    std::cout << " answer[" << answers[i].first << "] ";
+    if (answers[i].second) std::cout << *answers[i].second;
+    else std::cout << "NO OPERATION";
   }
-
-  std::cout << NL << NL << "------------ REPORT----------------";
-  for (auto const& [part, answers] : solution) {
-    std::cout << NL << "Part " << part << " answers";
-    for (auto const& [heading, answer] : answers) {
-      if (answer != 0) std::cout << NT << "answer[" << heading << "] " << answer;
-      else std::cout << NT << "answer[" << heading << "] " << " NO OPERATION ";
-    }
-  }
-  std::cout << NL << NL;
+  std::cout << "\n";
+  /*
+  For my input:
+  ANSWERS
+   ...
+  */
   return 0;
 }
