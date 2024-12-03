46b50a1090cf7e17b3cad83ff27b24a8bc95c648
kjelloh <kjell-olov.hogdahl@itfied.se>
2024-12-03 06:32:00 +0100

    day_3 part 1

---
+++ day_3/day_3.cpp
@@ -0,0 +1,148 @@
+#include <cctype>
+#include <iostream>
+#include <iomanip> // E.g., std::quoted
+#include <string>
+#include <string_view>
+#include <sstream> // E.g., std::istringstream, std::ostringstream
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <array>
+#include <ranges> // E.g., std::subrange, std::view
+#include <utility> // E.g., std::pair,..
+#include <algorithm> // E.g., std::find, std::all_of,...
+#include <numeric> // E.g., std::accumulate
+#include <limits> // E.g., std::numeric_limits
+#include <fstream>
+#include <format>
+#include <optional>
+#include <regex>
+
+auto const NL = "\n";
+auto const T = "\t";
+auto const NT = "\n\t";
+
+using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64 bit int: 9.22 x 10^18
+using Result = Integer;
+struct Expression {
+  std::string f;
+  int left_op;
+  int right_op;
+};
+using Model = std::vector<Expression>;
+
+Model parse(auto& in) {
+  std::cout << "\n<BEGIN parse>";
+  Model result{};
+  std::string line{};
+  while (std::getline(in,line)) {
+    std::regex pattern(R"(mul\((\d+),(\d+)\))"); // match 'mul(a,b)' into a,b subgroups
+    auto matches_begin = std::sregex_iterator(line.begin(), line.end(), pattern);
+    auto matches_end = std::sregex_iterator();
+    for (std::sregex_iterator iter = matches_begin; iter != matches_end; ++iter) {
+      std::smatch match = *iter;
+      std::string s = match.str();
+      std::cout << "\n\top:" << s;
+      Expression exp{};
+      exp.f = "mul";
+      exp.left_op = std::stoi(match[1].str());
+      exp.right_op = std::stoi(match[2].str());
+      std::cout << " --> " << std::quoted(exp.f) << " " << exp.left_op << " " << exp.right_op;
+      result.push_back(exp);
+    }
+  }
+  std::cout << "\n<END parse>";
+  return result;
+}
+
+using Args = std::vector<std::string>;
+
+namespace part1 {
+  std::optional<Result> solve_for(std::istream& in,Args const& args) {
+    std::optional<Result> result{};
+    std::cout << NL << NL << "part1";
+    if (in) {
+      Result acc{};
+      auto model = parse(in);
+      for (auto const& exp : model) {
+        acc += exp.left_op*exp.right_op;
+      }
+      result = acc;
+    }
+    return result;
+  }
+}
+
+namespace part2 {
+  std::optional<Result> solve_for(std::istream& in,Args const& args) {
+    std::optional<Result> result{};
+    std::cout << NL << NL << "part2";
+    if (in) {
+      auto model = parse(in);
+    }
+    return result;
+  }
+}
+
+using Answers = std::vector<std::pair<std::string,std::optional<Result>>>;
+int main(int argc, char *argv[])
+{
+  Args args{};
+  for (int i=0;i<argc;++i) {
+    args.push_back(argv[i]);
+  }
+  Answers answers{};
+  std::vector<std::chrono::time_point<std::chrono::system_clock>> exec_times{};
+  exec_times.push_back(std::chrono::system_clock::now());
+  for (int state=0;state<2;++state) {
+    switch (state) {
+      case 0: {
+        std::filesystem::path file{"../../example.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 1 Test",part1::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 1: {
+        std::filesystem::path file{"../../puzzle.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 1     ",part1::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 2: {
+        std::filesystem::path file{"../../example.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 2 Test",part2::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 3: {
+        std::filesystem::path file{"../../puzzle.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 2     ",part2::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      default:{std::cerr << "\nSORRY, no action for state " << state;} break;
+    }
+  }
+  
+  std::cout << "\n\nANSWERS";
+  for (int i=0;i<answers.size();++i) {
+    std::cout << "\nduration:" << std::chrono::duration_cast<std::chrono::milliseconds>(exec_times[i+1] - exec_times[i]).count() << "ms";
+    std::cout << " answer[" << answers[i].first << "] ";
+    if (answers[i].second) std::cout << *answers[i].second;
+    else std::cout << "NO OPERATION";
+  }
+  std::cout << "\n";
+  /*
+  For my input:
+  ANSWERS
+   ...
+  */
+  return 0;
+}
