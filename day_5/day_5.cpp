b09c563b0ef25179e5e949d2840b1169b121eaec
kjelloh <kjell-olov.hogdahl@itfied.se>
2024-12-05 07:38:05 +0100

    day_5 part 1

---
+++ day_5/day_5.cpp
@@ -0,0 +1,127 @@
+#include <cctype>
+#include <iostream>
+#include <iomanip> // E.g., std::quoted
+#include <string>
+#include <string_view>
+#include <sstream> // E.g., std::istringstream, std::ostringstream
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <array>
+#include <ranges> // E.g., std::subrange, std::view
+#include <utility> // E.g., std::pair,..
+#include <algorithm> // E.g., std::find, std::all_of,...
+#include <numeric> // E.g., std::accumulate
+#include <limits> // E.g., std::numeric_limits
+#include <fstream>
+#include <format>
+#include <optional>
+#include <regex>
+
+auto const NL = "\n";
+auto const T = "\t";
+auto const NT = "\n\t";
+
+using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64 bit int: 9.22 x 10^18
+using Result = Integer;
+using Model = std::vector<std::string>;
+
+Model parse(auto& in) {
+  std::cout << "\n<BEGIN parse>";
+  Model result{};
+  std::string line{};
+  while (std::getline(in,line)) {
+    std::cout << "\nLine:" << std::quoted(line);
+    result.push_back(line);
+  }
+  std::cout << "\n<END parse>";
+  return result;
+}
+
+using Args = std::vector<std::string>;
+
+namespace part1 {
+  std::optional<Result> solve_for(std::istream& in,Args const& args) {
+    std::optional<Result> result{};
+    std::cout << NL << NL << "part1";
+    if (in) {
+      auto model = parse(in);
+    }
+    return result;
+  }
+}
+
+namespace part2 {
+  std::optional<Result> solve_for(std::istream& in,Args const& args) {
+    std::optional<Result> result{};
+    std::cout << NL << NL << "part2";
+    if (in) {
+      auto model = parse(in);
+    }
+    return result;
+  }
+}
+
+using Answers = std::vector<std::pair<std::string,std::optional<Result>>>;
+int main(int argc, char *argv[])
+{
+  Args args{};
+  for (int i=0;i<argc;++i) {
+    args.push_back(argv[i]);
+  }
+  Answers answers{};
+  std::vector<std::chrono::time_point<std::chrono::system_clock>> exec_times{};
+  exec_times.push_back(std::chrono::system_clock::now());
+  std::vector<int> states = {0,1,2,3};
+  for (auto state : states) {
+    switch (state) {
+      case 0: {
+        std::filesystem::path file{"../../example.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 1 Example",part1::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 1: {
+        std::filesystem::path file{"../../puzzle.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 1     ",part1::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 2: {
+        std::filesystem::path file{"../../example.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 2 Example",part2::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 3: {
+        std::filesystem::path file{"../../puzzle.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 2     ",part2::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      default:{std::cerr << "\nSORRY, no action for state " << state;} break;
+    }
+  }
+  
+  std::cout << "\n\nANSWERS";
+  for (int i=0;i<answers.size();++i) {
+    std::cout << "\nduration:" << std::chrono::duration_cast<std::chrono::milliseconds>(exec_times[i+1] - exec_times[i]).count() << "ms";
+    std::cout << " answer[" << answers[i].first << "] ";
+    if (answers[i].second) std::cout << *answers[i].second;
+    else std::cout << "NO OPERATION";
+  }
+  std::cout << "\n";
+  /*
+  For my input:
+  ANSWERS
+   ...
+  */
+  return 0;
+}
