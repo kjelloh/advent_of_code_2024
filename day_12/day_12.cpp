3400d3f7458f0ad0118eb6869066d7e7d2d630fd
kjelloh <kjell-olov.hogdahl@itfied.se>
2024-12-12 18:08:23 +0100

    day_11 part 2 on the way

--- day_12/day_12.cpp
+++ day_12/day_12.cpp
@@ -78,6 +78,7 @@ using aoc::grid::Position;
 using aoc::grid::Positions;
 
 using Region = std::pair<char,Positions>;
+using Perimeter = Positions;
 
 std::vector<Position> to_adjacent(Position const& pos) {
     return {
@@ -101,16 +102,16 @@ std::vector<Position> to_nighbours(Position const& pos) {
     };
 }
 
-using RegionInfo = std::pair<Region, int>; // Region and perimeter count
+using RegionInfo = std::pair<Region, Perimeter>; // Region and perimeter
 
-Result to_perimeter(Region const& region,Grid const& grid) {
-  Result result{};
+Perimeter to_perimeter(Region const& region,Grid const& grid) {
+  Perimeter result{};
 
   auto const& [region_id,members] = region;
   for (auto const& pos : members) {
     for (auto const& neighbour : to_nighbours(pos)) {
       if (not grid.on_map(neighbour) or grid.at(neighbour).value() != region_id) {
-        ++result;
+        result.push_back(neighbour);
       }
     }
   }
@@ -158,11 +159,7 @@ RegionInfos compute_regions(Grid const& grid) {
     for (int col = 0; col < static_cast<int>(grid.width()); ++col) {
       Position pos = {row, col};
       if (!visited.count(pos)) {
-        // Perform flood-fill for unvisited positions
         auto region_info = flood_fill(grid, pos, visited);
-        char region_id = grid.at(pos).value();
-
-        // Update the map
         result.push_back(region_info);
       }
     }
@@ -178,13 +175,11 @@ namespace part1 {
     if (in) {
       auto model = parse(in);
       Grid grid{model};
-
       auto infos = compute_regions(grid);
-
       for (const auto& [region,perimeter] : infos) {
         auto id = region.first;
         auto area = region.second.size();
-        auto price = area*perimeter;
+        auto price = area*perimeter.size();
         std::cout << NL << "Plot ID: " << id;
         std::cout << NL << T << "Region:" << region.second;
         std::cout << NL << T << "Area:" << area;
@@ -195,17 +190,217 @@ namespace part1 {
       }
       result = acc;
     }
-    return result; // 1358716 is too high
-                   // 1352976
+    return result;
   }
 }
 
 namespace part2 {
+
+  // Directions for moving up, down, left, right
+  Position const RIGHT{0,1};
+  Position const DOWN{1,0};
+  Position const LEFT{0,-1};
+  Position const UP{-1,0};
+  using Direction = Position;
+  using Directions = Positions;
+  Directions const directions = {RIGHT,RIGHT+DOWN,DOWN,DOWN+LEFT,LEFT,LEFT+UP,UP,UP+RIGHT};
+
+  struct Edge {
+    Position start;
+    Position end;
+    Position normal; // Normal vector for the edge
+    
+    bool operator==(const Edge& other) const {
+      return (start == other.start && end == other.end and normal == other.normal);
+    }
+
+    bool operator<(const Edge& other) const {
+        if (start != other.start)
+            return start < other.start;
+        if (end != other.end)
+            return end < other.end;
+        return normal < other.normal;
+    }
+  };
+
+  std::ostream& operator<<(std::ostream& os,Edge const& edge) {
+    auto const& [start,end,normal] = edge;
+    os << "edge{start:" << start << ",end:" << end << ",normal:" << normal << "}";
+    return os;
+  }
+
+  using Edges = std::vector<Edge>;
+
+  std::ostream& operator<<(std::ostream& os,Edges const& edges) {
+    std::cout << "edges[";
+    for (auto const& edge : edges) {
+      if (edge != edges.front()) std::cout << "'";
+      std::cout << edge;
+    }
+    std::cout << "]";
+    return os;
+  }
+
+
+  Edges to_all_grid_pos_edges(Region const& region) {
+    std::cout << NL << "to_all_grid_pos_edges region:" << region.first << " : " << region.second.size();
+    Edges result{};
+    for (auto const& pos : region.second) {
+      // Represent edge as if upper left corner is at plot {row,col}
+      Edge top{pos,pos+RIGHT,UP};
+      Edge right{pos+RIGHT,pos+RIGHT+DOWN,RIGHT};
+      Edge bottom{pos+RIGHT+DOWN,pos+DOWN,DOWN};
+      Edge left{pos+DOWN,pos,LEFT};
+      result.push_back(top);
+      result.push_back(right);
+      result.push_back(bottom);
+      result.push_back(left);
+    }
+    return result;
+  }
+
+  Edge to_reversed(Edge const& edge) {
+    auto [start,end,normal] = edge;
+    Direction reversed_normal{-normal.row,-normal.col};
+    return {end,start,reversed_normal};
+  }
+
+  Edges to_perimeter_grid_pos_edges(Edges const& edges) {
+    std::cout << NL << "to_perimeter_grid_pos_edges edges:" << edges.size();
+    Edges result{};
+    std::set<Edge> outer_edges{};
+    for (auto const& edge : edges) {
+      auto reversed_edge = to_reversed(edge);
+      if (outer_edges.contains(reversed_edge)) {
+        outer_edges.erase(reversed_edge);
+      }
+      else {
+        outer_edges.insert(edge);
+      }
+    }
+    result.assign(outer_edges.begin(), outer_edges.end());
+    return result;
+  }
+
+  class DisconnectedGraph {
+  public:
+      // Constructor to process edges and find disconnected graphs
+      DisconnectedGraph(Edges const& edges) {
+        std::cout << NL << "DisconnectedGraph(edges:" << edges.size() << ")";
+        findDisconnectedGraphs(edges);
+      }
+
+      // Get the resulting subgraphs
+      std::vector<Edges> const& getSubgraphs() const {
+          return subgraphs;
+      }
+
+  private:
+      std::vector<Edges> subgraphs{};
+
+      std::vector<int> parent{};
+      std::vector<int> rank{};
+
+      int find(int v) {
+          if (parent[v] != v)
+              parent[v] = find(parent[v]); // Path compression
+          return parent[v];
+      }
+
+      void unionSets(int u, int v) {
+          int rootU = find(u);
+          int rootV = find(v);
+          if (rootU != rootV) {
+              if (rank[rootU] < rank[rootV]) {
+                  parent[rootU] = rootV;
+              } else if (rank[rootU] > rank[rootV]) {
+                  parent[rootV] = rootU;
+              } else {
+                  parent[rootV] = rootU;
+                  ++rank[rootU];
+              }
+          }
+      }
+
+      void findDisconnectedGraphs(Edges const& edges) {
+          // Step 1: Collect all unique vertices
+          std::map<Position, int> vertexToId;
+          int vertexId = 0;
+
+          for (const auto& edge : edges) {
+              if (vertexToId.find(edge.start) == vertexToId.end())
+                  vertexToId[edge.start] = vertexId++;
+              if (vertexToId.find(edge.end) == vertexToId.end())
+                  vertexToId[edge.end] = vertexId++;
+          }
+
+          // Initialize union-find (disjoint set) structure
+          parent.resize(vertexId);
+          rank.resize(vertexId, 0);
+
+          for (int i = 0; i < vertexId; ++i)
+              parent[i] = i;
+
+          // Step 3: Union edges
+          for (const auto& edge : edges) {
+              int u = vertexToId[edge.start];
+              int v = vertexToId[edge.end];
+              unionSets(u, v);
+          }
+
+          // Step 4: Group edges by connected component
+          std::map<int, Edges> componentEdges;
+          for (const auto& edge : edges) {
+              int compId = find(vertexToId[edge.start]);
+              componentEdges[compId].push_back(edge);
+          }
+
+          // Step 5: Store the result
+          for (const auto& [_, component] : componentEdges) {
+              subgraphs.push_back(component);
+          }
+      }
+  };
+
+  Result to_side_count(Region const& region) {
+    std::cout << NL << "to_side_count id:" << region.first;
+    Result result{};
+    auto all_grid_pos_edges = to_all_grid_pos_edges(region);
+    auto perimeter_grid_pos_edges = to_perimeter_grid_pos_edges(all_grid_pos_edges);
+    DisconnectedGraph disconnected_graph{perimeter_grid_pos_edges};
+    std::cout << NL << "disconnected_graph count:" << disconnected_graph.getSubgraphs().size() << std::flush;
+    if (disconnected_graph.getSubgraphs().size()>1);
+    for (auto const& subgraph : disconnected_graph.getSubgraphs()) {
+      // TODO: Compress subgraph (edges) into straight edges of the region.
+      //       Edges start,end,start,end... with the same normal vector should be merged into sinlge start,end,normal
+      //       We can then just count the final region edges
+    }
+    return result+1;
+  }
+
   std::optional<Result> solve_for(std::istream& in,Args const& args) {
     std::optional<Result> result{};
     std::cout << NL << NL << "part2";
     if (in) {
       auto model = parse(in);
+      Grid grid{model};
+      auto infos = compute_regions(grid);
+      Result acc{};
+      for (const auto& [region,perimeter] : infos) {
+        auto id = region.first;
+        auto area = region.second.size();
+        auto side_count = to_side_count(region);
+        auto price = area*side_count;
+        std::cout << NL << "Plot ID: " << id;
+        std::cout << NL << T << "Region:" << region.second;
+        std::cout << NL << T << "Area:" << area;
+        std::cout << NL << T << "Perimeter: " << perimeter;
+        std::cout << NL << T << "side_count: " << side_count;
+        std::cout << NL << T << "Price: " << price;
+        std::cout << NL << "-------------------------";
+        acc += price;
+      }
+      result = acc;
     }
     return result;
   }
@@ -230,8 +425,8 @@ int main(int argc, char *argv[]) {
   Answers answers{};
   std::vector<std::chrono::time_point<std::chrono::system_clock>> exec_times{};
   exec_times.push_back(std::chrono::system_clock::now());
-  std::vector<int> states = {11,12,13,10};
-//  std::vector<int> states = {11,12,13,10,23,20};
+  std::vector<int> states = {21};
+  //  std::vector<int> states = {11,12,13,10,23,20};
   for (auto state : states) {
     switch (state) {
       case 11: {
@@ -262,8 +457,29 @@ int main(int argc, char *argv[]) {
         else std::cerr << "\nSORRY, no file " << file;
         exec_times.push_back(std::chrono::system_clock::now());
       } break;
-      case 23: {
+      case 21: {
         std::filesystem::path file{working_dir / "example.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 2 Larger Example",part2::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 22: {
+        std::filesystem::path file{working_dir / "example2.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 2 Larger Example",part2::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 23: {
+        std::filesystem::path file{working_dir / "example3.txt"};
+        std::ifstream in{file};
+        if (in) answers.push_back({"Part 2 Larger Example",part2::solve_for(in,args)});
+        else std::cerr << "\nSORRY, no file " << file;
+        exec_times.push_back(std::chrono::system_clock::now());
+      } break;
+      case 24: {
+        std::filesystem::path file{working_dir / "example4.txt"};
         std::ifstream in{file};
         if (in) answers.push_back({"Part 2 Larger Example",part2::solve_for(in,args)});
         else std::cerr << "\nSORRY, no file " << file;
@@ -290,8 +506,13 @@ int main(int argc, char *argv[]) {
   std::cout << "\n";
   /*
   For my input:
-  ANSWERS
-   ...
-  */
+
+   ANSWERS
+   duration:0ms answer[Part 1 Example] 140
+   duration:0ms answer[Part 1 Example 2] 772
+   duration:2ms answer[Part 1 Larger Example] 1930
+   duration:322ms answer[Part 1     ] 1352976
+
+   */
   return 0;
 }
