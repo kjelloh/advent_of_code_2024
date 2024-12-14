e2f9452a60b63d8483fd52e9fb89603be24a682e
kjelloh <kjell-olov.hogdahl@itfied.se>
2024-12-14 08:19:23 +0100

    day_14 part 1 not yet correct

--- day_14/day_14.cpp
+++ day_14/day_14.cpp
@@ -48,25 +48,48 @@ auto const NT = "\n\t";
 
 using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64 bit int: 9.22 x 10^18
 using Result = Integer;
-using Model = aoc::parsing::Lines;
+
+using aoc::xy::Vector;
+using Robot = std::pair<Vector,Vector>;
+using Robots = std::vector<Robot>;
+std::ostream& operator<<(std::ostream& os,Robot const& robot) {
+  std::cout << "{p:" << robot.first << ",v:" << robot.second << "}";
+  return os;
+}
+std::ostream& operator<<(std::ostream& os,Robots const& robots) {
+  std::cout << "[";
+  std::string sep{};
+  for (auto const& robot : robots) {
+    std::cout << sep << robot;
+    if (sep.size()==0) sep = "\n\t,";
+  }
+  return os;
+  std::cout << "]";
+}
+
+using Model = Robots;
 
 Model parse(auto& in) {
   using namespace aoc::parsing;
   Model result{};
   auto input = Splitter{in};
   auto lines = input.lines();
-  if (lines.size()>1) {
-    std::cout << NL << T << lines.size() << " lines";
-    for (int i=0;i<lines.size();++i) {
-      auto line = lines[i];
-      std::cout << NL << T << T << "line[" << i << "]:" << line.size() << " " << std::quoted(line.str());
-      result.push_back(line);
+  std::cout << NL << T << lines.size() << " lines";
+  for (int i=0;i<lines.size();++i) {
+    auto line = lines[i];
+    std::cout << NL << T << T << "line[" << i << "]:" << line.size() << " " << std::quoted(line.str());
+    // "p=20,54 v=57,17"
+    auto groups = line.groups(R"(p=([+-]?\d+),([+-]?\d+) v=([+-]?\d+),([+-]?\d+))");
+    if (groups.size()==4) {
+      auto px = std::stoi(groups[0].str());
+      auto py = std::stoi(groups[1].str());
+      auto vx = std::stoi(groups[2].str());
+      auto vy = std::stoi(groups[3].str());
+      result.push_back({{px,py},{vx,vy}});
     }
-  }
-  else {
-    // single line
-    std::cout << NL << T << T << "input:" << input.size() << " " << std::quoted(input.str());
-    result.push_back(input.trim());
+    else {
+      std::cerr << NL << "PARSE ERROR: WRong numbers count: " << groups.size() << " in " << std::quoted(line.str());
+    }
   }
   return result;
 }
@@ -79,6 +102,73 @@ namespace part1 {
     std::cout << NL << NL << "part1";
     if (in) {
       auto model = parse(in);
+      std::cout << NL << "robots:" << model;
+      // Allow to solve for both example and puzzle
+      // In my input and example there is always a robot at left most and bottom most.
+      auto [p_max,_] = std::accumulate(model.begin(), model.end(), Robot{},[](auto acc,auto const& robot){
+        auto [p,v] = robot;
+        Vector max_p = {std::max(acc.first.x,p.x),std::max(acc.first.y,p.y)};
+        acc.first = max_p;
+        return acc;
+      });
+      auto width = p_max.x+1;
+      auto height = p_max.y+1;
+      std::cout << NL << "width:" << width << " height:" << height;
+      
+      int const STEPS{100};
+      auto transformed = model;
+      std::for_each(transformed.begin(), transformed.end(), [width,height,STEPS](Robot& robot){
+        
+        auto [p,v] = robot;
+        auto [x,y] = p;
+        auto [vx,vy] = v;
+        
+        auto end_x = (x + STEPS*vx) % width; // Wrap around
+        if (end_x<0) end_x += width;
+        auto end_y = (y + STEPS*vy) % height;
+        if (end_y<0) end_y += height;
+        
+        std::cout << NL << T << "robot:" << robot << " --> ";
+                
+        robot.first.x = end_x;
+        robot.first.y = end_y;
+        
+        std::cout << robot;
+      });
+      std::cout << NL << "transformed:" << transformed;
+
+      std::array<Result,4> acc{};
+      auto in_quadrant = [width,height](Robot const& robot,int ix){
+        // assume odd width and height
+        auto ghosted_x = (width+1) / 2; // 11 -> 6, 101 -> 51
+        auto ghosted_y = (height+1) / 2; // 7 -> 4, 103 -> 52
+        auto [p,v] = robot;
+        auto [px,py] = p;
+        if (px < ghosted_x and py < ghosted_y and ix ==1) return true;
+        if (px > ghosted_x and py < ghosted_y and ix ==3) return true;
+        if (px < ghosted_x and py > ghosted_y and ix ==2) return true;
+        if (px > ghosted_x and py > ghosted_y and ix ==4) return true;
+        return false;
+      };
+
+      for (auto const& robot : transformed) {
+        std::cout << NL << "robot:" << robot;
+        for (int i=1;i<=4;++i) {
+          if (in_quadrant(robot, i)) {
+            std::cout << " In quadrant " << i;
+            ++acc[i-1];
+            break;
+          }
+        }
+      }
+      
+      std::cout << NL << "quadrant counts";
+      for (int i=1;i<=4;++i) {
+        std::cout << NL << T << "acc[" << i << "]:" << acc[i-1];
+      }
+
+      result = acc[0]*acc[1]*acc[2]*acc[3];
+        
     }
     return result;
   }
@@ -115,7 +205,7 @@ int main(int argc, char *argv[]) {
   std::vector<std::chrono::time_point<std::chrono::system_clock>> exec_times{};
   exec_times.push_back(std::chrono::system_clock::now());
 //  std::vector<int> states = {11};
-  std::vector<int> states = {11,10,21,20};
+  std::vector<int> states = {11};
   for (auto state : states) {
     switch (state) {
       case 11: {
