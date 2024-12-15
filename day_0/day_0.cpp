1a1caa93b05db7d1517ac66ee4c234939025e7d2
kjelloh <kjell-olov.hogdahl@itfied.se>
2024-12-15 21:10:21 +0100

    day_0 cpp-template updated for test against log file

--- day_0/day_0.cpp
+++ day_0/day_0.cpp
@@ -24,6 +24,10 @@
 #include <regex>
 #include <filesystem>
 
+using aoc::raw::NL;
+using aoc::raw::T;
+using aoc::raw::NT;
+
 // Try to read the path to the actual working directory
 // from a text file at the location where we execute
 std::optional<std::filesystem::path> get_working_dir() {
@@ -42,9 +46,20 @@ std::optional<std::filesystem::path> get_working_dir() {
   return result;
 }
 
-auto const NL = "\n";
-auto const T = "\t";
-auto const NT = "\n\t";
+std::filesystem::path to_working_dir_path(std::string const& file_name) {
+  static std::optional<std::filesystem::path> cached{};
+  if (not cached) {
+    cached = "../..";
+    if (auto dir = get_working_dir()) {
+      cached = *dir;
+    }
+    else {
+      std::cout << NL << "No working directory path configured";
+    }
+    std::cout << NL << "Using working_dir " << *cached;
+  }
+  return *cached / file_name;
+}
 
 using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64 bit int: 9.22 x 10^18
 using Result = Integer;
@@ -72,6 +87,52 @@ Model parse(auto& in) {
 }
 
 using Args = std::vector<std::string>;
+
+namespace test {
+
+  using LogEntry = std::string;
+
+  std::ostream& operator<<(std::ostream& os,LogEntry const& entry) {
+    return os;
+  }
+  using LogEntries = std::vector<LogEntry>;
+  std::ostream& operator<<(std::ostream& os,LogEntries const& log) {
+    return os;
+  }
+
+  LogEntries parse(auto& in) {
+    std::cout << NL << T << "test::parse";
+    LogEntries result{};
+    using namespace aoc::parsing;
+    auto input = Splitter{in};
+    auto lines = input.lines();
+    if (lines.size()>1) {
+      std::cout << NL << T << lines.size() << " lines";
+      for (int i=0;i<lines.size();++i) {
+        auto line = lines[i];
+        std::cout << NL << T << T << "line[" << i << "]:" << line.size() << " " << std::quoted(line.str());
+      }
+    }
+    else {
+      // single line
+      std::cout << NL << T << T << "input:" << input.size() << " " << std::quoted(input.str());
+    }
+    return result;
+  }
+
+  std::optional<Result> test0(Args args) {
+    std::cout << NL << NL << "test0";
+    return std::nullopt;
+  }
+
+  std::optional<Result> test1(auto& in, auto& log_in,Args args) {
+    std::cout << NL << NL << "test1";
+    auto model = ::parse(in);
+    auto log = test::parse(log_in);
+    return std::nullopt;
+  }
+
+}
 
 namespace part1 {
   std::optional<Result> solve_for(std::istream& in,Args const& args) {
@@ -102,52 +163,50 @@ int main(int argc, char *argv[]) {
     args.push_back(argv[i]);
   }
 
-  std::filesystem::path working_dir{"../.."};
-  if (auto dir = get_working_dir()) {
-    working_dir = *dir;
-  }
-  else {
-    std::cout << NL << "No working directory path configured";
-  }
-  std::cout << NL << "Using working_dir " << working_dir;
-
   Answers answers{};
   std::vector<std::chrono::time_point<std::chrono::system_clock>> exec_times{};
   exec_times.push_back(std::chrono::system_clock::now());
-//  std::vector<int> states = {11};
-  std::vector<int> states = {11,10,21,20};
+  std::vector<int> states = {0,111,11};
   for (auto state : states) {
     switch (state) {
+      case 0: {
+        answers.push_back({"test0",test::test0(args)});
+      } break;
+      case 111: {
+        auto log_file = to_working_dir_path("example.log");
+        std::ifstream log_in{log_file};
+        auto file = to_working_dir_path("example.txt");
+        std::ifstream in{file};
+        if (in and log_in) answers.push_back({"Part 1 Test Example vs Log",test::test1(in,log_in,args)});
+        else std::cerr << "\nSORRY, no file " << file << " or log_file " << log_file;
+      } break;
       case 11: {
-        std::filesystem::path file{working_dir / "example.txt"};
+        auto file = to_working_dir_path("example.txt");
         std::ifstream in{file};
         if (in) answers.push_back({"Part 1 Example",part1::solve_for(in,args)});
         else std::cerr << "\nSORRY, no file " << file;
-        exec_times.push_back(std::chrono::system_clock::now());
       } break;
       case 10: {
-        std::filesystem::path file{working_dir / "puzzle.txt"};
+        auto file = to_working_dir_path("puzzle.txt");
         std::ifstream in{file};
         if (in) answers.push_back({"Part 1     ",part1::solve_for(in,args)});
         else std::cerr << "\nSORRY, no file " << file;
-        exec_times.push_back(std::chrono::system_clock::now());
       } break;
       case 21: {
-        std::filesystem::path file{working_dir / "example.txt"};
+        auto file = to_working_dir_path("example.txt");
         std::ifstream in{file};
         if (in) answers.push_back({"Part 2 Example",part2::solve_for(in,args)});
         else std::cerr << "\nSORRY, no file " << file;
-        exec_times.push_back(std::chrono::system_clock::now());
       } break;
       case 20: {
-        std::filesystem::path file{working_dir / "puzzle.txt"};
+        auto file = to_working_dir_path("puzzle.txt");
         std::ifstream in{file};
         if (in) answers.push_back({"Part 2     ",part2::solve_for(in,args)});
         else std::cerr << "\nSORRY, no file " << file;
-        exec_times.push_back(std::chrono::system_clock::now());
       } break;
       default:{std::cerr << "\nSORRY, no action for state " << state;} break;
     }
+    exec_times.push_back(std::chrono::system_clock::now());
   }
   
   std::cout << "\n\nANSWERS";
@@ -159,9 +218,11 @@ int main(int argc, char *argv[]) {
   }
   std::cout << "\n";
   /*
-  For my input:
-  ANSWERS
+   For my input:
+
+   ANSWERS
    ...
+      
   */
   return 0;
 }
