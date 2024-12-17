c2dadeaf48e6aea14ee7af581602560b5edf8020
kjelloh <kjell-olov.hogdahl@itfied.se>
2024-12-17 08:06:13 +0100

    day_17 scaffolding up to execution but no action yet

--- day_17/day_17.cpp
+++ day_17/day_17.cpp
@@ -29,26 +29,229 @@ using aoc::raw::T;
 using aoc::raw::NT;
 
 using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64 bit int: 9.22 x 10^18
-using Result = Integer;
-using Model = aoc::raw::Lines;
+using Result = std::string;
+
+using Registers = std::map<char,int>;
+std::ostream& operator<<(std::ostream& os,Registers const& registers) {
+  std::cout << "registers:";
+  
+  return os;
+}
+
+enum Op {
+  op_unknown
+  ,adv = 0
+  ,bxl = 1
+  ,bst = 2
+  ,jnz = 3
+  ,bxc = 4
+  ,out = 5
+  ,bdv = 6
+  ,cdv = 7
+  ,op_undefined
+};
+
+Op to_op(int op) {return static_cast<Op>(op);}
+
+std::string to_op_name(Op op) {
+//  The eight instructions are as follows:
+//
+  switch (op) {
+
+//  The adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator is found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an integer and then written to the A register.
+    case adv: return "adv";break;
+
+//
+//  The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.
+    case bxl: return "bxl";break;
+//
+//  The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
+    case bst: return "bst";break;
+//
+//  The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
+    case jnz: return "jnz";break;
+//
+//  The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
+    case bxc: return "bxc";break;
+//
+//  The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If a program outputs multiple values, they are separated by commas.)
+    case out: return "out";break;
+//
+//  The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register. (The numerator is still read from the A register.)
+    case bdv: return "bdv";break;
+//
+//  The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)
+    case cdv: return "cdv";break;
+
+    default: return "?";break;
+  }
+}
+
+using Statement = std::pair<Op,int>;
+std::ostream& operator<<(std::ostream& os,Statement const& statement) {
+  std::cout << "statement:";
+  std::cout << "{" << statement.first << ":" << to_op_name(statement.first) << "," << statement.second << "}";
+  return os;
+}
+using Program = std::vector<Statement>;
+std::ostream& operator<<(std::ostream& os,Program const& program) {
+  std::cout << "program:";
+  for (auto const& statement : program) {
+    std::cout << NL << T << statement;
+  }
+  return os;
+}
+
+std::string to_op_description(Op op) {
+//  The eight instructions are as follows:
+//
+  switch (op) {
+
+//  The adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator is found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an integer and then written to the A register.
+    case adv: return "The adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator is found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an integer and then written to the A register";break;
+
+//
+//  The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.
+    case bxl: return "The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B";break;
+//
+//  The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
+    case bst: return "The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register";break;
+//
+//  The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
+    case jnz: return "The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction pointer is not increased by 2 after this instruction";break;
+//
+//  The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
+    case bxc: return "The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)";break;
+//
+//  The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If a program outputs multiple values, they are separated by commas.)
+    case out: return "The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If a program outputs multiple values, they are separated by commas.)";break;
+//
+//  The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register. (The numerator is still read from the A register.)
+    case bdv: return "The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register. (The numerator is still read from the A register.)";break;
+//
+//  The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)
+    case cdv: return "The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)";break;
+
+    default: return "?";break;
+  }
+}
+
+struct CPU {
+  Registers reg{};
+  std::string operator()(Statement const& statement) {
+    std::string result{};
+    auto [op,operand] = statement;
+    std::cout << NL << "execute:" << statement;
+    switch (op) {
+        
+        //  The adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator is found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an integer and then written to the A register.
+      case adv: {
+        std::cout << NL << to_op_description(op);
+        std::cout << NL << T << "NOT YET IMPLEMENTED";
+      } break;
+        
+        //
+        //  The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.
+      case bxl: {
+        std::cout << NL << to_op_description(op);
+        std::cout << NL << T << "NOT YET IMPLEMENTED";
+      } break;
+        //
+        //  The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
+      case bst: {
+        std::cout << NL << to_op_description(op);
+        std::cout << NL << T << "NOT YET IMPLEMENTED";
+      } break;
+        //
+        //  The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
+      case jnz: {
+        std::cout << NL << to_op_description(op);
+        std::cout << NL << T << "NOT YET IMPLEMENTED";
+      } break;
+        //
+        //  The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
+      case bxc: {
+        std::cout << NL << to_op_description(op);
+        std::cout << NL << T << "NOT YET IMPLEMENTED";
+      } break;
+        //
+        //  The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If a program outputs multiple values, they are separated by commas.)
+      case out: {
+        std::cout << NL << to_op_description(op);
+        std::cout << NL << T << "NOT YET IMPLEMENTED";
+      } break;
+        //
+        //  The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register. (The numerator is still read from the A register.)
+      case bdv: {
+        std::cout << NL << to_op_description(op);
+        std::cout << NL << T << "NOT YET IMPLEMENTED";
+      } break;
+        //
+        //  The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)
+      case cdv: {
+        std::cout << NL << to_op_description(op);
+        std::cout << NL << T << "NOT YET IMPLEMENTED";
+      } break;
+        
+      default: {
+        std::cout << NL << T << "UNKNOWN OPERATOR - NOP";
+      } break;
+    }
+    return result;
+  }
+  bool operator==(CPU const& other) const = default;
+};
+std::ostream& operator<<(std::ostream& os,CPU const& cpu) {
+  std::cout << "cpu:";
+  std::cout << cpu.reg;
+  return os;
+}
+
+struct Computer {
+  CPU cpu{};
+  Program prog{};
+  Result run() {
+    Result result{};
+    std::cout << NL << "run:" << cpu << " on " << prog;
+    for (auto const& statement : prog) {
+      result += cpu(statement);
+    }
+    return result;
+  }
+  bool operator==(Computer const& other) const = default;
+};
+
+std::ostream& operator<<(std::ostream& os,Computer const& pc) {
+  std::cout << "pc:";
+  std::cout << pc.cpu << pc.prog;
+  return os;
+}
+
+struct Model {
+  Registers registers{};
+  Program program{};
+};
+
+std::ostream& operator<<(std::ostream& os,Model const& model) {
+  std::cout << "model:" << model.registers << model.program;
+  return os;
+}
 
 Model parse(auto& in) {
   using namespace aoc::parsing;
+  using aoc::raw::operator<<;
   Model result{};
   auto input = Splitter{in};
-  auto lines = input.lines();
-  if (lines.size()>1) {
-    std::cout << NL << T << lines.size() << " lines";
-    for (int i=0;i<lines.size();++i) {
-      auto line = lines[i];
-      std::cout << NL << T << T << "line[" << i << "]:" << line.size() << " " << std::quoted(line.str());
-      result.push_back(line);
+  auto sections = input.sections();
+  if (sections.size()==2) {
+    for (int i=0;i<sections.size();++i) {
+      std::cout << NL << "------ section " << i << " -------";
+      auto const& section = sections[i];
+      std::cout << to_raw(section);
     }
   }
   else {
-    // single line
-    std::cout << NL << T << T << "input:" << input.size() << " " << std::quoted(input.str());
-    result.push_back(input.trim());
+    std::cerr << "Sorry, Expected two sections but got " << sections.size();
   }
   return result;
 }
@@ -59,41 +262,71 @@ namespace test {
 
   // Adapt to expected for day puzzle
   struct LogEntry {
+    Computer pc{};
+    std::optional<Result> output{};
     bool operator==(LogEntry const& other) const {
       bool result{true};
+      result = result and (pc == other.pc);
+      result = result and (output and other.output)?(*output==*other.output):true;
       return result;
     }
   };
 
   std::ostream& operator<<(std::ostream& os,LogEntry const& entry) {
-    return os;
+    std::cout << NL << "log entry:";
+    std::cout << entry.pc;
+    if (entry.output) std::cout << *entry.output;
+    else std::cout << "?";
   }
 
   using LogEntries = aoc::test::LogEntries<LogEntry>;
 
-  LogEntries parse(auto& in) {
+  LogEntries parse0(auto& in) {
     std::cout << NL << T << "test::parse";
     LogEntries result{};
     using namespace aoc::parsing;
     auto input = Splitter{in};
     auto lines = input.lines();
-    if (lines.size()>1) {
-      std::cout << NL << T << lines.size() << " lines";
-      for (int i=0;i<lines.size();++i) {
-        auto line = lines[i];
-        std::cout << NL << T << T << "line[" << i << "]:" << line.size() << " " << std::quoted(line.str());
-      }
-    }
-    else {
-      // single line
-      std::cout << NL << T << T << "input:" << input.size() << " " << std::quoted(input.str());
+    std::cout << NL << T << lines.size() << " lines";
+    for (int i=0;i<lines.size();++i) {
+      auto line = lines[i];
+      std::cout << NL << T << T << "line[" << i << "]:" << line.size() << " " << std::quoted(line.str());
     }
     return result;
   }
 
+  LogEntries parse1(auto& in) {
+    std::cout << NL << T << "test::parse";
+    LogEntries result{};
+    using namespace aoc::parsing;
+    auto input = Splitter{in};
+    auto lines = input.lines();
+    std::cout << NL << T << lines.size() << " lines";
+    for (int i=0;i<lines.size();++i) {
+      auto line = lines[i];
+      std::cout << NL << T << T << "line[" << i << "]:" << line.size() << " " << std::quoted(line.str());
+    }
+    return result;
+  }
+
+
   std::optional<Result> test0(Args args) {
+    std::optional<Result> result{};
     std::cout << NL << NL << "test0";
-    return std::nullopt;
+    std::ifstream log_in{aoc::to_working_dir_path("example0.log")};
+    if (log_in) {
+      auto model = test::parse0(log_in);
+      {
+        // If register C contains 9, the program 2,6 would set register B to 1.
+        Registers reg{{'C',9}};
+        CPU cpu{reg};
+        Statement statement{to_op(2),6};
+        Program program{{statement}};
+        Computer pc{cpu,program};
+        auto output = pc.run();
+      }
+    }
+    return result;
   }
 
   std::optional<Result> test1(auto& in, auto& log_in,Args args) {
@@ -102,7 +335,7 @@ namespace test {
     if (in) {
       auto model = ::parse(in);
       if (log_in) {
-        auto log = test::parse(log_in);
+        auto log = test::parse1(log_in);
       }
     }
     return result;
@@ -142,7 +375,7 @@ int main(int argc, char *argv[]) {
   Answers answers{};
   std::vector<std::chrono::time_point<std::chrono::system_clock>> exec_times{};
   exec_times.push_back(std::chrono::system_clock::now());
-  std::vector<int> states = {0,111,11};
+  std::vector<int> states = {0,111,11,10};
   for (auto state : states) {
     switch (state) {
       case 0: {
