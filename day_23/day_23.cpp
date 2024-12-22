f2f1583967d5df09b1c839e040ff540a421ace26
kjelloh <kjell-olov.hogdahl@itfied.se>
2024-12-22 22:08:39 +0100

    Initial commit for Day 23

---
+++ day_23/day_23.cpp
@@ -0,0 +1,186 @@
+#include "aoc.hpp"
+
+#include <cctype>
+#include <iostream>
+#include <iomanip> // E.g., std::quoted
+#include <string>
+#include <string_view>
+#include <sstream> // E.g., std::istringstream, std::ostringstream
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <array>
+#include <ranges> // E.g., std::subrange, std::view
+#include <utility> // E.g., std::pair,..
+#include <algorithm> // E.g., std::find, std::all_of,...
+#include <numeric> // E.g., std::accumulate
+#include <limits> // E.g., std::numeric_limits
+#include <fstream>
+#include <format>
+#include <optional>
+#include <regex>
+#include <filesystem>
+
+using aoc::raw::NL;
+using aoc::raw::T;
+using aoc::raw::NT;
+
+using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64 bit int: 9.22 x 10^18
+using Result = aoc::raw::Line;
+using Model = aoc::raw::Lines;
+
+Model parse(auto& in) {
+  using namespace aoc::parsing;
+  Model result{};
+  auto sections = Splitter{in}.sections();
+  for (auto const& [sx,section] : aoc::views::enumerate(sections)) {
+    std::cout << NL << "---------- section " << sx << " ----------";
+    for (auto const& [lx,line] : aoc::views::enumerate(section)) {
+      std::cout << NL << T << T << "line[" << lx << "]:" << line.size() << " " << std::quoted(line.str());
+      result.push_back(line);
+    }
+  }
+  return result;
+}
+
+struct Args {
+  std::map<std::string,std::string> arg{};
+  std::set<std::string> options{};
+};
+
+namespace test {
+
+  // Adapt to expected for day puzzle
+  struct LogEntry {
+    bool operator==(LogEntry const& other) const {
+      bool result{true};
+      return result;
+    }
+  };
+
+  std::ostream& operator<<(std::ostream& os,LogEntry const& entry) {
+    return os;
+  }
+
+  using LogEntries = aoc::test::LogEntries<LogEntry>;
+
+  LogEntries parse_doc() {
+    std::cout << NL << T << "parse puzzle doc text";
+    LogEntries result{};
+    using namespace aoc::parsing;
+    std::ifstream doc_in{aoc::to_working_dir_path("doc.txt")};
+    auto sections = Splitter{doc_in}.same_indent_sections();
+    for (auto const& [sx,section] : aoc::views::enumerate(sections)) {
+      std::cout << NL << "---------- section " << sx << " ----------";
+      for (auto const& [lx,line] : aoc::views::enumerate(section)) {
+        std::cout << NL << T << T << "line[" << lx << "]:" << line.size() << " " << std::quoted(line.str());
+      }
+    }
+    return result;
+  }
+
+  std::optional<Result> solve_for(std::istream& in,Args const& args) {
+    std::optional<Result> result{};
+    std::cout << NL << NL << "test";
+    if (in) {
+      auto model = parse(in);
+    }
+    return result;
+  }
+
+}
+
+namespace part1 {
+  std::optional<Result> solve_for(std::istream& in,Args const& args) {
+    std::optional<Result> result{};
+    std::cout << NL << NL << "part1";
+    if (in) {
+      auto model = parse(in);
+    }
+    return result;
+  }
+}
+
+namespace part2 {
+  std::optional<Result> solve_for(std::istream& in,Args const& args) {
+    std::optional<Result> result{};
+    std::cout << NL << NL << "part2";
+    if (in) {
+      auto model = parse(in);
+    }
+    return result;
+  }
+}
+
+using Answers = std::vector<std::pair<std::string,std::optional<Result>>>;
+std::vector<Args> to_requests(Args const& args) {
+  std::vector<Args> result{};
+  result.push_back(args); // No fancy for now
+  return result;
+}
+int main(int argc, char *argv[]) {
+  Args user_args{};
+  
+  user_args.arg["part"] = 1;
+  user_args.arg["file"] = "example.txt";
+
+  // Override by any user input
+  for (int i=1;i<argc;++i) {
+    std::string token{argv[i]};
+    if (token.starts_with("-")) user_args.options.insert(token);
+    else {
+      // assume options before <part> and <file>
+      auto non_option_index = i - user_args.options.size(); // <part> <file>
+      switch (non_option_index) {
+        case 1: user_args.arg["part"] = token; break;
+        case 2: user_args.arg["file"] = token; break;
+        default: std::cerr << NL << "Unknown argument " << std::quoted(token);
+      }
+    }
+  }
+
+  
+  auto requests = to_requests(user_args);
+
+  Answers answers{};
+  std::vector<std::chrono::time_point<std::chrono::system_clock>> exec_times{};
+  exec_times.push_back(std::chrono::system_clock::now());
+  for (auto request : requests) {
+    auto part = request.arg["part"];
+    auto file = aoc::to_working_dir_path(request.arg["file"]);
+    std::ifstream in{file};
+    if (in) {
+      if (part=="1") {
+        answers.push_back({std::format("part{} {}",part,file.filename().string()),part1::solve_for(in,request)});
+      }
+      else if (part=="2") {
+        answers.push_back({std::format("part{} {}",part,file.filename().string()),part2::solve_for(in,request)});
+      }
+      else if (part=="test") {
+        answers.push_back({std::format("{} {}",part,file.filename().string()),test::solve_for(in,request)});
+      }
+    }
+    else std::cerr << "\nSORRY, no file " << file;
+    exec_times.push_back(std::chrono::system_clock::now());
+  }
+  
+  std::cout << "\n\nANSWERS";
+  for (int i=0;i<answers.size();++i) {
+    std::cout << "\nduration:" << std::chrono::duration_cast<std::chrono::milliseconds>(exec_times[i+1] - exec_times[i]).count() << "ms";
+    std::cout << " answer[" << answers[i].first << "] ";
+    if (answers[i].second) std::cout << *answers[i].second;
+    else std::cout << "NO OPERATION";
+  }
+  std::cout << "\n";
+  /*
+   For my input:
+
+   ANSWERS
+   ...
+      
+  */
+  return 0;
+}
