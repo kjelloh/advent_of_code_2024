bfc9e6f3f92691c8f7acb6b229e154bd206c4d0e
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-01-11 11:31:22 +0100

    day_18 refactored to template day_0 aoc::application

--- day_18/day_18.cpp
+++ day_18/day_18.cpp
@@ -36,6 +36,18 @@ using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64
 using Result = std::string;
 using Model = Positions;
 
+template <>
+struct std::formatter<Model> : std::formatter<std::string> {
+  template<class FmtContext>
+  FmtContext::iterator format(Model const& model, FmtContext& ctx) const {
+    std::ostringstream oss{};
+    oss << model;
+    std::format_to(ctx.out(),"\n{}",oss.str());
+    return ctx.out();
+  }
+};
+
+
 Model parse(auto& in) {
   using namespace aoc::parsing;
   Model result{};
@@ -136,13 +148,6 @@ namespace test {
     Grid unwalked{};
     Grid walked{};
     int best_step_count{-1};
-    bool operator==(Expected const& other) const {
-      bool result{true};
-//      result = result and (unwalked == other.unwalked);
-//      result = result and (walked == other.walked);
-      result = result and (best_step_count == other.best_step_count);
-      return result;
-    }
   };
 
   std::ostream& operator<<(std::ostream& os,Expected const& entry) {
@@ -154,7 +159,7 @@ namespace test {
     return os;
   }
 
-  using Expecteds = aoc::test::Expecteds<Expected>;
+//  using Expecteds = aoc::test::Expecteds<Expected>;
 
   std::vector<aoc::raw::Lines> to_examples(aoc::parsing::Sections const& sections) {
     std::vector<aoc::raw::Lines> result{};
@@ -163,118 +168,68 @@ namespace test {
     return result;
   }
 
-  Expecteds to_expecteds(aoc::parsing::Sections const& sections,auto config_ix,Args const& args) {
-    Expecteds result{};
-    
-    switch (config_ix) {
-      case 0: {
-        Expected expected{};
-        expected.byte_count = -1;
-        expected.best_step_count = -1;
-        {
-          //---------- section 15 ----------
-          //    line[0]:71 "   In the above example, if you were to draw the memory space after the"
-          //    line[1]:72 "   first 12 bytes have fallen (using . for safe and # for corrupted), it"
-          //    line[2]:24 "   would look like this:"
-          std::string entry = aoc::parsing::to_line(sections[15]).str();
-          std::cout << NL << T << std::quoted(entry);
-          std::regex pattern{R"(the first (\d+) bytes)"};
-          std::smatch match{};
-          if (std::regex_search(entry,match,pattern)) {
-            expected.byte_count = std::stoi(match[1]);
-            std::cout << " --> byte_count:" << expected.byte_count;
-          }
-        }
-        {
-          expected.unwalked.base() = aoc::parsing::to_raw(sections[16]);
-        }
-        {
-          //---------- section 17 ----------
-          //    line[0]:72 "   You can take steps up, down, left, or right. After just 12 bytes have"
-          //    line[1]:71 "   corrupted locations in your memory space, the shortest path from the"
-          //    line[2]:72 "   top left corner to the exit would take 22 steps. Here (marked with O)"
-          //    line[3]:20 "   is one such path:"
-          std::string entry = aoc::parsing::to_line(sections[17
-                                                             ]).str();
-          std::cout << NL << T << std::quoted(entry);
-          std::regex sc_pattern{R"(would take (\d+) steps)"};
-          std::smatch match{};
-          if (std::regex_search(entry,match,sc_pattern)) {
-            expected.best_step_count = std::stoi(match[1]);
-            std::cout << " --> best_step_count:" << expected.best_step_count;
-          }
-        }
-        {
-          expected.walked.base() = aoc::parsing::to_raw(sections[18]);
-        }
-        result.push_back(expected);
-      } break;
-      default: std::cout << NL << "Sorry, config_ix " << config_ix << " not recognized - Ignored";
-    }
-    
-    
-    return result;
-  }
-
-
-  std::optional<Result> test1(Model const& model,Expecteds const& expecteds,Args args) {
-    std::optional<Result> result{};
-    std::cout << NL << NL << "test1";
-    if (expecteds.size()==1) {
-      auto& expected = expecteds[0];
-      std::cout << NL << expected;
-      auto const& unwalked = expected.unwalked;
-      auto best_path = to_best_path(unwalked.top_left(),unwalked.bottom_right(), unwalked);
-      std::cout << NL << "best_path:" << best_path;
-      auto best_step_count = best_path.size()-1;
-      std::cout << NL << "best_step_count:" << best_step_count;
-      auto computed = to_unwalked(model,expected.byte_count);
-      computed = aoc::grid::to_filled(computed, best_path);
-      std::cout << NL << "computed:" << computed;
-      if (best_step_count == expected.best_step_count) {
-        result = "PASSED";
-      }
-      else {
-        result = "FAILED";
+  aoc::application::ExpectedTeBool test1(std::optional<aoc::parsing::Sections> const& opt_sections,Args args) {
+    if (not opt_sections) return std::unexpected("doc.txt required");
+    auto const& sections = *opt_sections;
+    std::cout << NL << T << "sections ok";
+    auto examples = to_examples(sections);
+    if (examples.size()==0) return std::unexpected("example from doc.txt required");
+    std::cout << NL << T << "examples ok";
+    auto example_in = aoc::test::to_example_in(examples[0]);
+    auto example_model = parse(example_in);
+    std::cout << NL << std::format("\n{}",example_model);
+        
+    Expected expected{};
+    expected.byte_count = -1;
+    expected.best_step_count = -1;
+    {
+      //---------- section 15 ----------
+      //    line[0]:71 "   In the above example, if you were to draw the memory space after the"
+      //    line[1]:72 "   first 12 bytes have fallen (using . for safe and # for corrupted), it"
+      //    line[2]:24 "   would look like this:"
+      std::string entry = aoc::parsing::to_line(sections[15]).str();
+      std::cout << NL << T << std::quoted(entry);
+      std::regex pattern{R"(the first (\d+) bytes)"};
+      std::smatch match{};
+      if (std::regex_search(entry,match,pattern)) {
+        expected.byte_count = std::stoi(match[1]);
+        std::cout << " --> byte_count:" << expected.byte_count;
       }
     }
-    else {
-      std::cout << NL << "Sorry, Expected exactly one 'expected'";
+    {
+      expected.unwalked.base() = aoc::parsing::to_raw(sections[16]);
     }
-    return result;
-  }
-
-  std::optional<Result> solve_for(std::istream& in,Args const& args) {
-    std::ostringstream response{};
-    std::cout << NL << NL << "test";
-    if (in) {
-      auto model = parse(in);
-      auto doc = aoc::doc::parse_doc(args);
-      auto examples = to_examples(doc);
-      for (auto const& [ix,example_lines] : aoc::views::enumerate(examples)) {
-        if (args.options.contains("-to_example")) {
-          auto example_file = aoc::to_working_dir_path(std::format("example{}.txt",ix));
-          if (aoc::raw::write_to_file(example_file, example_lines)) {
-            response << "Created " << example_file;
-          }
-          else {
-            response << "Sorry, failed to create file " << example_file;
-          }
-        }
-        else {
-          std::ostringstream oss{};
-          aoc::raw::write_to(oss, example_lines);
-          std::istringstream example_in{oss.str()};
-          auto example_model = ::parse(example_in);
-          std::cout << NL << NL << "example_model:" << example_model;
-          auto expecteds = test::to_expecteds(doc, ix,args);
-          /* Call tests here */
-          return test1(model,expecteds,args);
-        }
+    {
+      //---------- section 17 ----------
+      //    line[0]:72 "   You can take steps up, down, left, or right. After just 12 bytes have"
+      //    line[1]:71 "   corrupted locations in your memory space, the shortest path from the"
+      //    line[2]:72 "   top left corner to the exit would take 22 steps. Here (marked with O)"
+      //    line[3]:20 "   is one such path:"
+      std::string entry = aoc::parsing::to_line(sections[17
+                                                         ]).str();
+      std::cout << NL << T << std::quoted(entry);
+      std::regex sc_pattern{R"(would take (\d+) steps)"};
+      std::smatch match{};
+      if (std::regex_search(entry,match,sc_pattern)) {
+        expected.best_step_count = std::stoi(match[1]);
+        std::cout << " --> best_step_count:" << expected.best_step_count;
       }
     }
-    if (response.str().size()>0) return response.str();
-    else return std::nullopt;
+    {
+      expected.walked.base() = aoc::parsing::to_raw(sections[18]);
+    }
+
+    
+    std::cout << NL << expected;
+    auto const& unwalked = expected.unwalked;
+    auto best_path = to_best_path(unwalked.top_left(),unwalked.bottom_right(), unwalked);
+    std::cout << NL << "best_path:" << best_path;
+    auto best_step_count = best_path.size()-1;
+    std::cout << NL << "best_step_count:" << best_step_count;
+    auto computed = to_unwalked(example_model,expected.byte_count);
+    computed = aoc::grid::to_filled(computed, best_path);
+    std::cout << NL << "computed:" << computed;
+    return (best_step_count == expected.best_step_count);
   }
 
 } // namespace test
@@ -353,99 +308,29 @@ namespace part2 {
   }
 }
 
-using Answers = std::vector<std::pair<std::string,std::optional<Result>>>;
-
-std::vector<Args> to_requests(Args const& args) {
-  std::vector<Args> result{};
-  result.push_back(args); // No fancy for now
-  return result;
-}
-
 int main(int argc, char *argv[]) {
-  Args user_args{};
-  
-  // Override by any user input
-  for (int i=1;i<argc;++i) {
-    user_args.arg["file"] = "example.txt";
-    std::string token{argv[i]};
-    if (token.starts_with("-")) user_args.options.insert(token);
-    else {
-      // assume options before <part> and <file>
-      auto non_option_index = i - user_args.options.size(); // <part> <file>
-      switch (non_option_index) {
-        case 1: user_args.arg["part"] = token; break;
-        case 2: user_args.arg["file"] = token; break;
-        default: std::cerr << NL << "Unknown argument " << std::quoted(token);
-      }
-    }
-  }
-  
-  auto requests = to_requests(user_args);
-  
-  if (not user_args or user_args.options.contains("-all")) {
-    requests.clear();
-    
-    std::vector<std::tuple<std::set<std::string>,std::string,std::string>> states{
-       {{},"test","example.txt"}
-      ,{{},"1","example.txt"}
-      ,{{},"1","puzzle.txt"}
-      ,{{},"2","example.txt"}
-      ,{{},"2","puzzle.txt"}
-    };
-    
-    for (const auto& [options,part, file] : states) {
-      Args args;
-      if (options.size()>0) args.options = options;
-      args.arg["part"] = part;
-      if (file.size()>0) args.arg["file"] = file;
-      requests.push_back(args);
-    }
-  }
-
-  Answers answers{};
-  std::vector<std::chrono::time_point<std::chrono::system_clock>> exec_times{};
-  exec_times.push_back(std::chrono::system_clock::now());
-  for (auto request : requests) {
-    auto part = request.arg["part"];
-    auto file = aoc::to_working_dir_path(request.arg["file"]);
-    std::cout << NL << "Using part:" << part << " file:" << file;
-    std::ifstream in{file};
-    if (in) {
-      if (part=="1") {
-        answers.push_back({std::format("part{} {}",part,file.filename().string()),part1::solve_for(in,request)});
-      }
-      else if (part=="2") {
-        answers.push_back({std::format("part{} {}",part,file.filename().string()),part2::solve_for(in,request)});
-      }
-      else if (part.starts_with("test")) {
-        answers.push_back({std::format("{} {}",part,file.filename().string()),test::solve_for(in,request)});
-      }
-    }
-    else std::cerr << "\nSORRY, no file " << file;
-    exec_times.push_back(std::chrono::system_clock::now());
-  }
-  
-  std::cout << "\n\nANSWERS";
-  for (int i=0;i<answers.size();++i) {
-    std::cout << "\nduration:" << std::chrono::duration_cast<std::chrono::milliseconds>(exec_times[i+1] - exec_times[i]).count() << "ms";
-    std::cout << " answer[" << answers[i].first << "] ";
-    if (answers[i].second) std::cout << *answers[i].second;
-    else std::cout << "NO OPERATION";
-  }
-  std::cout << "\n";
+  aoc::application app{};
+  app.add_to_examples(test::to_examples);
+  app.add_test("test1", test::test1);
+  app.add_solve_for("1",part1::solve_for,"example.txt");
+  app.add_solve_for("1",part1::solve_for,"puzzle.txt");
+  app.add_solve_for("2",part2::solve_for,"example.txt");
+  app.add_solve_for("2",part2::solve_for,"puzzle.txt");
+  app.run(argc, argv);
+  app.print_result();
   /*
-
+   
    Xcode Debug -O2
 
    >day_18 -all
-   
+      
    ANSWERS
-   duration:4ms answer[test example.txt] PASSED
-   duration:0ms answer[part1 example.txt] 22
-   duration:71ms answer[part1 puzzle.txt] 252
-   duration:0ms answer[part2 example.txt] 6,1
-   duration:21390ms answer[part2 puzzle.txt] 5,60
-
+   duration:2ms answer[part:"test1"] PASSED
+   duration:0ms answer[part 1 in:example.txt] 22
+   duration:73ms answer[part 1 in:puzzle.txt] 252
+   duration:1ms answer[part 2 in:example.txt] 6,1
+   duration:21554ms answer[part 2 in:puzzle.txt] 5,60
+   
    */
-  return 0;
+
 }
