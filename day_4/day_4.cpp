d90392c581356fa50b34fb8c648b81183062eb83
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-01-12 21:58:17 +0100

    day_4 refactored to template day_0 aoc::application

--- day_4/day_4.cpp
+++ day_4/day_4.cpp
@@ -1,3 +1,5 @@
+#include "aoc.hpp"
+
 #include <cctype>
 #include <iostream>
 #include <iomanip> // E.g., std::quoted
@@ -26,7 +28,7 @@ auto const T = "\t";
 auto const NT = "\n\t";
 
 using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64 bit int: 9.22 x 10^18
-using Result = Integer;
+using Result = std::string;
 using Model = std::vector<std::string>;
 
 Model parse(auto& in) {
@@ -40,8 +42,6 @@ Model parse(auto& in) {
   std::cout << "\n<END parse>";
   return result;
 }
-
-using Args = std::vector<std::string>;
 
 struct Vector {
   int row;
@@ -71,7 +71,7 @@ char at_pos(Vector const& pos,Model const& model) {
 namespace part1 {
   std::optional<Result> solve_for(std::istream& in,Args const& args) {
     std::optional<Result> result{};
-    Result acc{0};
+    Integer acc{0};
     std::cout << NL << NL << "part1";
     if (in) {
       auto model = parse(in);
@@ -105,7 +105,7 @@ namespace part1 {
           } // dirs
         }
       }
-      result = acc;
+      result = std::to_string(acc);
     }
     return result;
   }
@@ -114,7 +114,7 @@ namespace part1 {
 namespace part2 {
   std::optional<Result> solve_for(std::istream& in,Args const& args) {
     std::optional<Result> result{};
-    Result acc{0};
+    Integer acc{0};
     std::cout << NL << NL << "part1";
     if (in) {
       auto model = parse(in);
@@ -150,72 +150,35 @@ namespace part2 {
           }
         }
       }
-      result = acc;
+      result = std::to_string(acc);
     }
     return result;
   }
 }
 
-using Answers = std::vector<std::pair<std::string,std::optional<Result>>>;
-int main(int argc, char *argv[])
-{
-  Args args{};
-  for (int i=0;i<argc;++i) {
-    args.push_back(argv[i]);
-  }
-  Answers answers{};
-  std::vector<std::chrono::time_point<std::chrono::system_clock>> exec_times{};
-  exec_times.push_back(std::chrono::system_clock::now());
-  std::vector<int> states = {0,1,2,3};
-  for (auto state : states) {
-    switch (state) {
-      case 0: {
-        std::filesystem::path file{"../../example.txt"};
-        std::ifstream in{file};
-        if (in) answers.push_back({"Part 1 Example",part1::solve_for(in,args)});
-        else std::cerr << "\nSORRY, no file " << file;
-        exec_times.push_back(std::chrono::system_clock::now());
-      } break;
-      case 1: {
-        std::filesystem::path file{"../../puzzle.txt"};
-        std::ifstream in{file};
-        if (in) answers.push_back({"Part 1     ",part1::solve_for(in,args)});
-        else std::cerr << "\nSORRY, no file " << file;
-        exec_times.push_back(std::chrono::system_clock::now());
-      } break;
-      case 2: {
-        std::filesystem::path file{"../../example.txt"};
-        std::ifstream in{file};
-        if (in) answers.push_back({"Part 2 Example",part2::solve_for(in,args)});
-        else std::cerr << "\nSORRY, no file " << file;
-        exec_times.push_back(std::chrono::system_clock::now());
-      } break;
-      case 3: {
-        std::filesystem::path file{"../../puzzle.txt"};
-        std::ifstream in{file};
-        if (in) answers.push_back({"Part 2     ",part2::solve_for(in,args)});
-        else std::cerr << "\nSORRY, no file " << file;
-        exec_times.push_back(std::chrono::system_clock::now());
-      } break;
-      default:{std::cerr << "\nSORRY, no action for state " << state;} break;
-    }
-  }
-  
-  std::cout << "\n\nANSWERS";
-  for (int i=0;i<answers.size();++i) {
-    std::cout << "\nduration:" << std::chrono::duration_cast<std::chrono::milliseconds>(exec_times[i+1] - exec_times[i]).count() << "ms";
-    std::cout << " answer[" << answers[i].first << "] ";
-    if (answers[i].second) std::cout << *answers[i].second;
-    else std::cout << "NO OPERATION";
-  }
-  std::cout << "\n";
+int main(int argc, char *argv[]) {
+  aoc::application app{};
+  app.add_solve_for("1",part1::solve_for,"example.txt");
+  app.add_solve_for("1",part1::solve_for,"puzzle.txt");
+  app.add_solve_for("2",part2::solve_for,"example.txt");
+  app.add_solve_for("2",part2::solve_for,"puzzle.txt");
+  app.run(argc, argv);
+  app.print_result();
   /*
-  For my input:
+
+   Xcode Debug -O2
+
+   >day_4 -all
+   
+   For my input:
+               
    ANSWERS
-   duration:3ms answer[Part 1 Example] 18
-   duration:697ms answer[Part 1     ] 2401
-   duration:1ms answer[Part 2 Example] 9
-   duration:523ms answer[Part 2     ] 1822   
+   duration:2ms answer[part 1 in:example.txt] 18
+   duration:79ms answer[part 1 in:puzzle.txt] 2401
+   duration:0ms answer[part 2 in:example.txt] 9
+   duration:33ms answer[part 2 in:puzzle.txt] 1822
+
    */
   return 0;
+
 }
